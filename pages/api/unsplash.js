8e66ddab6b446ede0d97b4892006bd025c77f080
Jan Bína <binajohny@gmail.com>
2021-04-02 12:13:03 +0200

    Custom random

--- pages/api/unsplash.js
+++ pages/api/unsplash.js
@@ -1,16 +1,21 @@
 
 export default async (_, res) => {
+    const maxPhoto = 1000
+    const page = 1 + Math.floor(Math.random() * maxPhoto)
+    
     const auth = "Client-ID " + process.env.UNSPLASH_ACCESS_KEY
+
     const resX = await fetch(
-        `https://api.unsplash.com/photos/random?query=bull&count=1&orientation=landscape`,
+        `https://api.unsplash.com/search/photos?query=bull&orientation=landscape&per_page=1&page=${page}`,
         {
             headers: {
                 Authorization: auth,
             },
         }
     );
-    const responseJson = await resX.json()    
+    const responseJson = await resX.json()
+    const results = responseJson.results
 
     res.setHeader('Cache-Control', 'public, max-age=3600');
-    return res.status(200).json(responseJson[0])
+    return res.status(200).json(results[0])
 };
